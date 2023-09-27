90ddac0174f0a54ea32445eeb95125fc8c820113
Guillermo <guillezuhe@gmail.com>
2023-09-27 11:56:42 +0200

    random number test

--- src/core/forces.cpp
+++ src/core/forces.cpp
@@ -59,6 +59,7 @@
 
 #include <cassert>
 #include <cmath>
+#include <random>
 
 /** Initialize the forces for a ghost particle */
 inline ParticleForce init_ghost_force(Particle const &) { return {}; }
@@ -125,12 +126,19 @@ static void init_forces(const ParticleRange &particles,
      set torque to zero for all and rescale quaternions
   */
   for (auto &p : particles) {
+    /*
+    RANDOM NUMBERS NEEDED
+    */
+    std::random_device rd;
+    std::mt19937 gen(red());
+    std::normal_distribution<> gaussianDist(0,1);
 
     /* Update of the viscoelastic force */
     for (int j = 0; j < 3; j++) {
       if ((!p.is_fixed_along(j)) && thermo_switch) {
+        double randomG = gaussianDist(gen);
         p.visc_force()[j] -= ((p.visc_force()[j] + p.qv() * abs(p.gamma()[j]) * p.v()[j]) * time_step + \
-        sqrt(2 * p.qv() * abs(p.gamma()[j]) * kT * time_step) * p.ext_torque()[j]) / p.taum();
+        sqrt(2 * p.qv() * abs(p.gamma()[j]) * kT * time_step) * randomG) / p.taum();
       }
     }
     p.f = init_real_particle_force(p, time_step, kT);
